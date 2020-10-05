121e1ea6010c4690e455eb45bc6de62c3a72b158
Seyda1 <seydaybar@gmail.com>
2020-10-05 09:16:22 +0300

    upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.WriteLine("Ebob için iki tamsayıyı giriniz");
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.Write(fonk(x, y));
+            Console.ReadLine();
+        }
+        static int fonk(int x, int y)
+        {
+            if (x % y == 0)
+                return y;
+            else
+                return (fonk(y,x % y));
+        
+        
+        
+        }
+    }
+}
